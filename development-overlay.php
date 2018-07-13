b5de7ac943619df86b304f9455b51f587fcba650
Sal Ferrarello <sal@ironcodestudio.com>
2018-07-13 15:34:06 -0400

    Initial commit

---
+++ development-overlay.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Development Overlay
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     development-overlay
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Development_Overlay
+ */
+
+// Your code starts here.
