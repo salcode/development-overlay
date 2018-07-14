115a9426c0a7eacf30c7357c63afae9bd7cede07
Sal Ferrarello <sal@ironcodestudio.com>
2018-07-14 06:34:39 -0400

    Add PHP and JavaScript for functionality

--- development-overlay.php
+++ development-overlay.php
@@ -1,15 +1,43 @@
 <?php
 /**
  * Plugin Name:     Development Overlay
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Plugin URI:      https://salferrarello.com/wordpress-development-overlay/
+ * Description:     Overlay an image on your page during development
+ * Author:          Sal Ferrarello
+ * Author URI:      https://salferrarello.com
  * Text Domain:     development-overlay
  * Domain Path:     /languages
  * Version:         0.1.0
  *
- * @package         Development_Overlay
+ * @package         development-overlay
  */
 
-// Your code starts here.
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+add_action( 'wp_enqueue_scripts', 'fe_development_overlay_enqueue_js' );
+
+/**
+ * Enqueue JavaScript
+ */
+function fe_development_overlay_enqueue_js() {
+	wp_enqueue_script(
+		'fe-development-overlay',
+		plugins_url( 'js/development-overlay.js', __FILE__ ),
+		array(),
+		'0.1.0',
+		true
+	);
+
+	wp_localize_script(
+		'fe-development-overlay',
+		// A name we makeup to avoid our PHP values colliding with others.
+		'feDevelopmentOverlay',
+		// A key/value array of values to pass from PHP to JavaScript.
+		array(
+			'path' => plugins_url( 'images', __FILE__ ),
+		)
+	);
+}
---
+++ js/development-overlay.js
@@ -0,0 +1,87 @@
+(function() {
+
+  var css = 'left: 50%; ',
+    src = 'default.jpg',
+
+    properties = {
+      'display': 'block',
+      'opacity': '0.5',
+      'margin-left': '0',
+      'position': 'absolute',
+      'top': '0',
+      'transform': 'translateX(-50%)',
+      'z-index': '99999'
+  },
+
+  propertiesThatNeedPx = [
+    'left',
+    'margin-left',
+    'top'
+  ],
+  image = document.createElement("img"),
+  toggleBtn = document.createElement("button");
+
+  // Update image src if present as URL param.
+  if ( getUrlParam( 'src' ) ) {
+    src = getUrlParam( 'src' );
+  }
+
+  // Add src to image.
+  image.src = feDevelopmentOverlay.path + '/' + src;
+
+  // Update CSS.
+  Object.keys(properties).forEach(function(key) {
+
+    // Get URL parameter.
+    // Use URL param 'left' for our 'margin-left' value.
+    var value = getUrlParam('margin-left' === key ? 'left' : key);
+
+    // Use default value from properties as fallback.
+    value = value ? value : properties[key];
+
+    // Add 'px' to the value if necessary.
+    value = needsPx(key) ? value + 'px' : value;
+
+    // Add to CSS string.
+    css += key + ': ' + value + '; ';
+  });
+
+  // Apply CSS styles.
+  image.style = css;
+
+  // Add image to page.
+  document.body.appendChild(image);
+
+  // Add toggle button.
+  toggleBtn.className = 'btn btn-warning';
+  toggleBtn.style = 'left: 20px; position: fixed; top: 80px; z-index: 99999;'
+  toggleBtn.textContent = 'Toggle';
+
+  // Listen for button click.
+  toggleBtn.addEventListener('click', toggleImageDisplay);
+
+  // Add button to page.
+  document.body.appendChild(toggleBtn);
+
+  function toggleImageDisplay() {
+    if ('block' === image.style.display) {
+      image.style.display = 'none';
+    } else {
+      image.style.display = 'block';
+    }
+  }
+
+  function getUrlParam( key ) {
+    var urlParams = new URLSearchParams(
+      window.location.href.substr(
+        window.location.href.indexOf('?') + 1
+      )
+    );
+    return urlParams.get( key );
+  }
+
+  function needsPx(key) {
+    return -1 !== propertiesThatNeedPx.indexOf(key);
+  }
+
+}());
